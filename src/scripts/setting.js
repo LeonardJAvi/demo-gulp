959e0d7701608dc42698342fb2c22779ac291f73
LeonardJAvi <informatic.leonardavila@gmail.com>
2018-07-09 10:06:55 -0400

    test gulp

---
+++ src/scripts/setting.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".single-item").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    infinite: true,
+    cssEase: 'linear',
+    fade: true,
+    arrows: true,
+    dots: true,
+    });
+});
+
+
